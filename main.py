34abc478452cc8b9124b5620dbd53f387d3f8c89
equi <git@notu.dev>
2023-01-20 21:49:45 -0500

    Support Server Passwords (#7)

--- main.py
+++ main.py
@@ -24,15 +24,16 @@ class bridge_: # 1:1 bridge:server
             self.c_map[d] = irc.irc_(self.ip, self.port, i, self)
             print("(irc)", i, "<--> (discord)", d) 
 
-    def __init__(self, ip, port, token, refresh):
-        self.ip = ip
-        self.port = port
-        self.token = token
-        self.refresh = refresh
+    def __init__(self, cfg):
+        self.ip = cfg["server"]
+        self.port = cfg["port"]
+        self.token = cfg["token"]
+        self.refresh = cfg["refresh"]
+        self.pword = cfg["pword"]
 
 def config():
     cfg = json.load(open("config.json")) 
-    b = bridge_(cfg["server"], cfg["port"], cfg["token"], cfg["refresh"]) # create bridge
+    b = bridge_(cfg) # create bridge
     b.load(cfg["channels"].items()) # load channel mapping
  
 if __name__ == "__main__":
